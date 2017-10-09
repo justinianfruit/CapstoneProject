db9c449a054adcf8817567e704ad081eb5f8cda1
Justine Barry <justinianfruit@gmail.com>
2017-10-09 07:53:20 -0600

    Finished Section 5 of Fullstack course, beginning personal site frame

--- FullstackCodeAlong/services/passport.js
+++ FullstackCodeAlong/services/passport.js
@@ -19,12 +19,12 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy: true
 }, (accessToken, refreshToken, profile, done) => {
     User.findOne({ googleId: profile.id })
         .then((existingUser) => {
             if (existingUser) {
-                //exists already
                 done(null, existingUser);
             } else {
                 new User({ googleId: profile.id }).save().then(user => done(null, user));
