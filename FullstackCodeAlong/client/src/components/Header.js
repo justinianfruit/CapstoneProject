a26f1574b77cda92e6d8e1f2867795ffba86660f
Justine Barry <justinianfruit@gmail.com>
2017-10-17 10:22:27 -0600

    Finished phase one

--- FullstackCodeAlong/client/src/components/Header.js
+++ FullstackCodeAlong/client/src/components/Header.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return;
+            case false:
+                return <li><a href="/auth/google">Login with Google</a></li>;
+            default:
+                return <li><a href="/api/logout">Logout</a></li>;
+        }
+    }
+
     render() {
         return (
             <nav>
-                <div class="nav-wrapper">
-                    <a class="left brand-logo">Emaily</a>
+                <div className="nav-wrapper">
+                    <Link 
+                    to={this.props.auth ? '/surveys' : '/'} 
+                    className="left brand-logo">Framing</Link>
                     <ul className="right">
-                        <li><a>Login with Google</a></li>
+                        {this.renderContent()}
                     </ul>
                 </div>
             </nav>
@@ -15,4 +30,8 @@ class Header extends Component {
     };
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
---
+++ FullstackCodeAlong/client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>Framing</h1>
+            Wireframe your product! Work independently or collaboratively!
+        </div>
+    );
+};
+
+export default Landing;
