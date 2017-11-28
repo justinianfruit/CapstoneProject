c21671138afff654b06a12bbc7785153e9396152
Justine Barry <justinianfruit@gmail.com>
2017-11-27 18:16:50 -0700

    Reducing repo size

--- react-pair-master/src/actions/challengesActions.js
+++
@@ -1,9 +0,0 @@
-
-export function getChallenges() {
-  const challenges = fetch(`${process.env.API_HOST}/api/v1/challenges`).then(res => {
-    return res.json()
-  }).then(res => {
-    return res
-  })
-  return {type: "GET_CHALLENGES", payload: challenges}
-}
